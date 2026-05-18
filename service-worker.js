69fc098ed43846f918d11a78bfa3154caba11493
alisonzheng12-stack <alisonzheng12@gmail.com>
2026-05-18 21:48:16 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "study-counter-app-20260518-09";
+﻿const CACHE_NAME = "study-counter-app-20260518-10";
 const ASSETS = [
   "./",
   "./index.html",
@@ -43,3 +43,4 @@ self.addEventListener("fetch", (event) => {
       .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
   );
 });
+
